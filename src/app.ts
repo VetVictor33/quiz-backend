84b235f28750605fdf81549a5250dc36ba827e7c
Victor Feliciano <115307935+VetVictor33@users.noreply.github.com>
2023-12-10 11:27:43 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
 import routes from './routes'
+import cors from 'cors'
 import { errorHandler } from './http/middlewares/error-handler'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(errorHandler)
